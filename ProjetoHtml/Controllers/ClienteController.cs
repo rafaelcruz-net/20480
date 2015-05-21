6f8037a62231669fd266a0d13285192b1ccf5be6
rafaelcruz-net <rafaelcruz_net@yahoo.com.br>
2015-05-21 15:31:10 -0700

    Comunicacao de Front End com BackEnd

---
+++ ProjetoHtml/Controllers/ClienteController.cs
@@ -0,0 +1,27 @@
+﻿using ProjetoHtml.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjetoHtml.Controllers
+{
+    public class ClienteController : Controller
+    {
+        //
+        // GET: /Cliente/
+
+        
+        [HttpPost]
+        public ActionResult Post(Cliente cliente)
+        {
+            //Faz algo//
+            // Valida cliente//
+            // Gravei o cliente//
+            cliente.Id = Guid.NewGuid();
+            return Json(cliente);
+        }
+
+    }
+}
---
+++ ProjetoHtml/Models/Cliente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjetoHtml.Models
+{
+    public class Cliente
+    {
+        public Guid? Id { get; set; }
+        public string Nome { get; set; }
+        public string Endereco { get; set; }
+        public string Email { get; set; }
+        public int Nacionalidade { get; set; }
+    }
+}
