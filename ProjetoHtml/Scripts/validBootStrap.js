d13637fd5958e95352abfa1d1ca3741beaa312c4
rafaelcruz-net <rafaelcruz_net@yahoo.com.br>
2015-05-20 17:34:05 -0700

    Validacao e Estilos de Mensagem

---
+++ ProjetoHtml/Scripts/validBootStrap.js
@@ -0,0 +1,21 @@
+﻿var validationClass = {
+    validateForm: function ($el, $msg) {
+        $($el).closest("div")
+              .removeClass("has-error");
+
+        $($el).closest("div").find("small").remove();
+
+        var isValid = true;
+
+        if ($($el).val() == "") {
+            $($el).closest("div")
+            .addClass("has-error");
+            isValid = false;
+            $($el).closest("div").append("<small class='error-msg'>" + $msg + "</small>");
+        }
+
+        return isValid;
+    }
+
+}
+
