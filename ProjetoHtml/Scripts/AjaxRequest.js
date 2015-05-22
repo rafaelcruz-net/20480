d85bd8dc2e342aad9e509ab3d80eab7b7b680853
rafaelcruz-net <rafaelcruz_net@yahoo.com.br>
2015-05-22 16:23:58 -0700

    Criacao do Promise e Ajax Request

---
+++ ProjetoHtml/Scripts/AjaxRequest.js
@@ -0,0 +1,16 @@
+﻿var AjaxRequest = function(url, type, data) {
+    this.url = url;
+    this.type = type;
+    this.data = data;
+
+    this.makeRequest = function () {
+
+        return $.ajax({
+            url: this.url,
+            contentType: "application/json; charset=utf8",
+            type: this.type,
+            data: JSON.stringify(this.data),
+        });
+    };
+
+}
